bbdedda94a2b51699e8b659cecf218f9ab2139a8
Andrew Wittrock <andy@wittrock.us>
2025-04-25 15:43:30 -0500

    Remove hard-coded config settings

--- src/config.ts
+++ src/config.ts
@@ -5,23 +5,8 @@ import { storeRoutes } from "./store/lib/routes";
 import {uacRoutes} from "@uac/lib/routes";
 
 export const config = ():IApiConfig => ({
-    appName: "EvilInnocence Store",
     api: {
         baseUrl: localConfig.api.baseUrl,
-    },
-    analytics: {
-        google: "G-G7SVPC1M02",
-        track: "https://wj0w5ct2yb.execute-api.us-east-1.amazonaws.com/production/analytics/pageview",
-        debug: false,
-    },
-    gallery: {
-        maxRowCount: 9999,
-    },
-    social: {
-        facebook: "DarkwyndChronicles",
-        twitter: "EvilInnocenceCG",
-        blueSky: "evilinnocence.bsky.social",
-        instagram: "evilinnocencecg",
     },
     modules: [
         "uac", "common", "admin"
@@ -29,12 +14,12 @@ export const config = ():IApiConfig => ({
     menus: [
     ],
     paypal: {
-        clientId: localConfig.paypal.clientId,
         plans: localConfig.paypal.plans,
     },
     routes: [
         ...publicRoutes.public,
         ...storeRoutes.public,
         ...uacRoutes.public,
-    ]
+    ],
+    settings: {}
 });
