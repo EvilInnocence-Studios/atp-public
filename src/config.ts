1aec863623b6c5673e0e41f33d95735917e194e2
Andrew Wittrock <andy@wittrock.us>
2025-06-26 23:06:17 -0500

    Get config values from modules

--- src/config.ts
+++ src/config.ts
@@ -1,25 +1,20 @@
-import { IConfig } from "@core/lib/types";
-import { publicRoutes } from "@public/routes";
+import { makeConfig } from "@core/lib/makeConfig";
 import { localConfig } from "./config.local";
-import { storeRoutes } from "./store/lib/routes";
-import {uacRoutes} from "@uac/lib/routes";
 
-export const config = ():IConfig => ({
-    api: {
-        baseUrl: localConfig.api.baseUrl,
-    },
-    modules: [
-        "uac", "common", "admin"
-    ],
-    menus: [
-    ],
-    paypal: {
-        plans: localConfig.paypal.plans,
-    },
-    routes: [
-        ...publicRoutes.public,
-        ...storeRoutes.public,
-        ...uacRoutes.public,
-    ],
-    settings: {}
-});
+// Import the modules you want active here
+import { module as commonModule } from "@common/index";
+import { module as publicModule } from "@public/index";
+import { module as storeModule } from "@store/index";
+import { module as subscriptionsModule } from "@subscription/index";
+import { module as uacModule } from "@uac/index";
+
+// List the modules you want active here
+const modules = [
+    publicModule,
+    commonModule,
+    uacModule,
+    subscriptionsModule,
+    storeModule,
+];
+
+export const config = makeConfig(localConfig.api.baseUrl, modules, "public");
