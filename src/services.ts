d5546265e3bbc43c9d24f6eaf93fc962d9888c00
Andrew Wittrock <andy@wittrock.us>
2025-07-26 12:55:57 -0500

    Add subscription services

--- src/services.ts
+++ src/services.ts
@@ -1,10 +1,12 @@
 import { commonServices } from "@common/lib/services";
 import { IMethods } from "@core/lib/types";
 import { storeServices } from "@store/services";
+import { subscriptionServices } from "@subscription/lib/services";
 import { uacServices } from "@uac/lib/services";
 
 export const apiServices = (methods:IMethods) => ({
     ...commonServices(methods),
     ...uacServices(methods),
     ...storeServices(methods),
+    ...subscriptionServices(methods),
 });
