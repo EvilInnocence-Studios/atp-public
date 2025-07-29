3bef25cdf27860f86561fc76054ae2138d81f00b
Andrew Wittrock <andy@wittrock.us>
2025-07-29 00:05:18 -0500

    Automatically update tsconfig aliases when installing modules

--- scripts/update-aliases.js
+++ scripts/update-aliases.js
@@ -2,12 +2,12 @@ import fs from 'fs';
 import path from 'path';
 
 // Update the tsconfig.json file to include aliases for the directories in the src directory
-const updateTsConfigAliases = (file:string) => {
-    const tsConfigPath = path.resolve(__dirname, file);
+const updateTsConfigAliases = (file) => {
+    const tsConfigPath = path.resolve(".", file);
     const tsConfig = JSON.parse(fs.readFileSync(tsConfigPath, 'utf-8'));
     
     // Dynamically generate the alias paths based on the directories in the src folder
-    const srcDir = path.resolve(__dirname, 'src');
+    const srcDir = path.resolve(".", 'src');
     const directories = fs.readdirSync(srcDir, { withFileTypes: true })
         .filter(dirent => dirent.isDirectory())
         .map(dirent => dirent.name);
@@ -33,6 +33,7 @@ const files = [
     'tsconfig.node.json',
     'tsconfig.app.json',
 ];
+console.log(`Updating tsconfig aliases`);
 files.forEach(file => {
     try {
         updateTsConfigAliases(file);
