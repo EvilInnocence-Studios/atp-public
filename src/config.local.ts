224deae5c3e655bb026819c3d9c13b942b8662f2
Andrew Wittrock <andy@wittrock.us>
2025-11-25 14:44:02 -0600

    Fix dependencies

--- src/config.local.ts
+++ src/config.local.ts
@@ -1,6 +1,6 @@
 export const localConfig = {
     api: {
-        baseUrl: window.location.origin === "http://localhost:3001"
+        baseUrl: window.location.origin === "http://localhost:3000"
             ? "http://localhost:3002/"
             : "https://<lambda-url-goes-here>.lambda-url.us-east-1.on.aws/",
     }
