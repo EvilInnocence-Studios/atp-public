9065bcb779b80e23120c1f62f462093ed3623b6a
Andrew Wittrock <andy@wittrock.us>
2026-02-18 21:17:41 -0600

    Add cloudfront script

--- src/config.local.ts
+++
@@ -1,7 +0,0 @@
-export const localConfig = {
-    api: {
-        baseUrl: window.location.origin === "http://localhost:3000"
-            ? "http://localhost:3002/"
-            : "https://<lambda-url-goes-here>.lambda-url.us-east-1.on.aws/",
-    }
-};
